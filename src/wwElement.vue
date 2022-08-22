116379e0612cb0477fabd7788ca4cba49faff343
Tom McDonough <tmcdonough@airslip.com>
2022-08-22 15:58:04 +0400

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -10,9 +10,9 @@ export default {
     okrawidget: Okra.buildWithOptions({
     name: 'Peter the Builder',
     env: 'production-sandbox',
-    app_id: 'https://app.okra.ng/q8ZToEIqq',
-    key: '2eaebfb3-a036-504c-8a2a-3cc192109ff1',
-    token: '60a24b9d80c1940297279802',
+    app_id: 'https://app.okra.ng/Ht63Wbkps',// app_id from your app builder
+    key: '2eaebfb3-a036-504c-8a2a-3cc192109ff1', // Your key from the Okra dashboard
+    token: '60a24b9d80c1940297279802', // Your token from the okra dashboard
     products: ['auth','identity','balance','transactions', 'income'], //in lowercase
     onSuccess: function(data){
         console.log('options success', data)
