2d61d2c831ff095c60b27ae11178b20fc42aff0a
Tom McDonough <tmcdonough@airslip.com>
2022-08-25 13:51:45 +0400

    Update wwSection.vue

--- src/wwSection.vue
+++ src/wwSection.vue
@@ -9,19 +9,13 @@ import Okra from 'okra-js'
 
 export default {
       methods: {
-        okrawidget: Okra.buildWithOptions({
-        name: 'Bnkability',
-        env: 'production-sandbox',
-        app_id: 'https://app.okra.ng/Ht63Wbkps',// app_id from your app builder
-        key: '2eaebfb3-a036-504c-8a2a-3cc192109ff1', // Your key from the Okra dashboard
-        token: '60a24b9d80c1940297279802', // Your token from the okra dashboard
-        products: ['auth','identity','balance','transactions', 'income'], //in lowercase
+        okrawidget: Okra.buildWithShortUrl({
+        short_url: 'izX93-Rx5',
         onSuccess: function(data){
-            // POST to API
+            //location.href = '/credit-readiness-financials?result=success';
         },
         onClose: function(){
-            // Go back 
-            //location.href = 'credit-readiness-financials?result=cancel';
+            //location.href = '/credit-readiness-financials?result=cancel';
         }
     })
   }
@@ -37,10 +31,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-body#okra-widget-body {
-    background-color: rgba(0,0,0,0.5) !important;
 }
 
 </style>
