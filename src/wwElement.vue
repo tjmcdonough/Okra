dcd5270a11fbc3e812be8088178252d1734d381c
Tom McDonough <tmcdonough@airslip.com>
2022-09-07 17:05:47 +0400

    Removed styling

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -32,15 +32,7 @@ export default {
 </script>
 
 <style>
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+  
 .next-button {
     font-family:"Bw Modelica", sans-serif;
     font-size: 14px;
